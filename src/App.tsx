f2e81061ff2e253eb5680616e275c92639d23294
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-01-11 21:28:25 +0300

    => save

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div>
+            <div>
+                <div>CSS</div>
+                <div>JS</div>
+                <div>React</div>
+                <div>HTML</div>
+            </div>
+        </div>
+    )
+}
+
+export default App
