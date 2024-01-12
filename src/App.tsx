aed645b60e0d1589ba2020dea803b6abf7a0d953
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-01-12 21:36:16 +0300

    => save

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,73 @@
 import './App.css'
 import {useEffect, useState} from 'react'
-// import axios from 'axios'
+import axios from 'axios'
 
 //===============================================================================================================================================================
 
-type tagsInputType = {id:number, title:string}
+type PhotoSizeViewModel = {
+    url: string;
+    width: number;
+    height: number;
+    fileSize: number;
+}
+
+type TodolistImagesViewModel = {
+    main: PhotoSizeViewModel[];
+}
+
+type TodolistViewModel = {
+    isImportant: boolean;
+    id: string;
+    title: string;
+    description: string;
+    addedDate: string;
+    order: number;
+    images: TodolistImagesViewModel;
+}
+
+type GetTodolistResponse = {
+    pagesCount: number;
+    page: number;
+    pageSize: number;
+    totalCount: number;
+    items: TodolistViewModel[];
+}
 
 //===============================================================================================================================================================
 
 function App() {
 
-    // сперва нет данных
-    const [tags, setTags] = useState<Array<tagsInputType>>([])
+    const [todoLists, setTodoLists] = useState<GetTodolistResponse | null>(null)
 
-    // после первой отрисовки без данных
-    // Апп получит даные с сервера - АСИНХРОННО и отрисует их
-    useEffect(()=> {
-        setTimeout(() => {
-            console.log('useEffect => COMPLETED')
-            setTags([
-                {id:1, title: 'CSS'},
-                {id:2, title: 'TS'},
-                {id:3, title: 'React'},
-                {id:4, title: 'HTML'}
-            ])
-        }, 2000)
+    useEffect(() => {
 
-        // функция будет запущена один раз, так как - deps: []
+        axios.get<GetTodolistResponse>('https://todolists.samuraijs.com/api/1.0/todolists?pageNumber=1&pageSize=10').then(response => {
+            console.log(response.data)
+            setTodoLists(response.data)
+        })
+
+
     }, [])
 
-    console.log('rendering => COMPLETED')
     return (
         <div>
             <div>
-                {tags.map(item => (<div key={item.id.toString()}>{item.title}</div>))}
+                {todoLists === null ?
+                    <h3>'loading'</h3>
+                    : todoLists.items.map(item => {
+                        const imageUrl = item.images.main.length > 1 ? item.images.main[1]?.url : 'http://placehold.co/48'
+                        return (
+                            <div key={item.id.toString()}>
+                                <img src={imageUrl} alt="sry"/>
+                                <h3>
+                                    {item.isImportant ? '✅' : ''}
+                                    {item.title}
+                                </h3>
+                                <div>{item.description}</div>
+                            </div>
+                        )
+
+                    })}
             </div>
         </div>
     )
